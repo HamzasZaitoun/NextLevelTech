2d635e111ef441a9a9b5b538a10b61ce5c891e69
Moawiah-Eqailan <meqailan5@gmail.com>
2024-10-25 15:30:49 +0300

    pull

---
+++ includes/usersClass.php
@@ -0,0 +1,52 @@
+<?php
+
+class User {
+    private $pdo;
+
+    public function __construct($pdo) {
+        $this->pdo = $pdo;
+    }
+
+    public function register($firstName, $lastName, $email, $password, $gender, $dob, $address, $phoneNumber) {
+        $sql = "INSERT INTO users (user_first_name, user_last_name, user_gender, user_birth_date, user_address, user_phone_number, user_email, user_password) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(1, $firstName);
+        $stmt->bindParam(2, $lastName);
+        $stmt->bindParam(3, $gender);
+        $stmt->bindParam(4, $dob);
+        $stmt->bindParam(5, $address);
+        $stmt->bindParam(6, $phoneNumber);
+        $stmt->bindParam(7, $email);
+        $stmt->bindParam(8, $password);
+
+        return $stmt->execute();
+    }
+
+    public function login($email, $password) {
+        $sql = "SELECT * FROM users WHERE user_email = :email";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':email', $email);
+        $stmt->execute();
+
+        $user = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        if ($user && $password == $user['user_password']) {
+            $_SESSION['user_id'] = $user['user_id'];
+            $_SESSION['user_email'] = $user['user_email'];
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    public function displayUserById($user_id) {
+        $sql = "SELECT * FROM users WHERE user_id = :user_id";
+        $stmt = $this->pdo->prepare($sql);
+        $stmt->bindParam(':user_id', $user_id);
+        $stmt->execute();
+
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
+};
+
--- login/loginCode.php
+++ login/loginCode.php
@@ -1,33 +1,19 @@
 <?php
 session_start();
-include('connect.php');
+include('../includes/db_connection.php');
+include ('../includes/usersClass.php');
+
+$user = new User($pdo);
 
 if ($_SERVER['REQUEST_METHOD'] == 'POST' && isset($_POST['email']) && isset($_POST['password'])) {
-
     $email = $_POST['email'];
     $password = $_POST['password'];
 
-
-
-
-    $sql = "SELECT `user_email`,`user_password` FROM `users` where user_email=:user_email";
-    $stmt = $connect->prepare($sql);
-
-    $stmt->bindParam('user_email', $email);
-
-    $stmt->execute();
-
-    $row = $stmt->fetch(PDO::FETCH_ASSOC);
-    if ($row) {
-        if ($password == $row['user_password']) {
-            $_SESSION['user_email'] = $row['user_email'];
-            $_SESSION['user_password'] = $row['user_password'];
-
-            header("location: https://www.facebook.com/");
-        } else {
-            $loginError = 'Invalid email or password.';
-            header("location: https://www.google.com/");
-        }
+    if ($user->login($email, $password)) {
+        header("Location: /project-php/NextLevelTech/index.php");
+        exit();
+    } else {
+        header("Location: login.php?error=Invalid email or password");
+        exit();
     }
 }
-?>
