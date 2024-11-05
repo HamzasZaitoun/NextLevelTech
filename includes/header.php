1db5c6acbbcbf6d7a1593d3e240cab32381bca05
mays-alkhalil <mays-alkhalil>
2024-11-05 16:01:58 +0300

    pull

--- includes/header.php
+++ includes/header.php
@@ -13,6 +13,11 @@ if (session_status() == PHP_SESSION_NONE) {
     <meta name="description" content="" />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
     <link rel="shortcut icon" type="image/x-icon" href="assets/images/logo/1302365453812760631remix-1730585196342.png" />
+    <head>
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
+</head>
 
     <!-- ========================= CSS here ========================= -->
     <link rel="stylesheet" href="assets/css/bootstrap.min.css" />
