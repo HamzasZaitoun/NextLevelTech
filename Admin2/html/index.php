65a6eddc1e61bbc3a02205bd1ccdc2eb39be5596
hamzaszaitoun <hamzasaadzaitoun@gmail.com>
2024-11-07 09:58:13 +0300

    fixing bugs

--- Admin2/html/index.php
+++ Admin2/html/index.php
@@ -198,7 +198,7 @@ $activeUserCount = $user->countActiveUsers();
                
                 <div class="row">
                     <!-- Column -->
-                    <div class="col-lg-8">
+                    <div class="col-lg-12">
                         <div class="card">
                             <div class="card-body">
                                 <div class="row">
@@ -231,38 +231,7 @@ $activeUserCount = $user->countActiveUsers();
                             </div>
                         </div>
                     </div>
-                    <div class="col-lg-4">
-                        <div class="card">
-                            <div class="card-body">
-                                <h3 class="card-title">Our Visitors </h3>
-                                <h6 class="card-subtitle">Different Devices Used to Visit</h6>
-                                <div id="visitor"
-                                    style="height: 290px; width: 100%; max-height: 290px; position: relative;"
-                                    class="c3">
-                                    <div class="c3-tooltip-container"
-                                        style="position: absolute; pointer-events: none; display: none;">
-                                    </div>
-                                </div>
-                            </div>
-                            <div>
-                                <hr class="mt-0 mb-0">
-                            </div>
-                            <div class="card-body text-center ">
-                                <ul class="list-inline d-flex justify-content-center align-items-center mb-0">
-                                    <li class="me-4">
-                                        <h6 class="text-info"><i class="fa fa-circle font-10 me-2 "></i>Mobile</h6>
-                                    </li>
-                                    <li class="me-4">
-                                        <h6 class=" text-primary"><i class="fa fa-circle font-10 me-2"></i>Desktop</h6>
-                                    </li>
-                                    <li class="me-4">
-                                        <h6 class=" text-success"><i class="fa fa-circle font-10 me-2"></i>Tablet</h6>
-                                    </li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+                   
              
                 <!-- ============================================================== -->
 
@@ -282,6 +251,9 @@ $activeUserCount = $user->countActiveUsers();
     <!-- End Wrapper -->
     <!-- ============================================================== -->
     <!-- ============================================================== -->
-    <?php 
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+  <?php 
  include "includes/footer.php";
  ?>
+ 
