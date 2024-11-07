853ac725b301c69f22d684e0ba106c926a226a38
hamzaszaitoun <hamzasaadzaitoun@gmail.com>
2024-11-07 09:38:12 +0300

    fixing bugs

--- Admin2/html/includes/footer.php
+++ Admin2/html/includes/footer.php
@@ -23,6 +23,7 @@
     <script src="js/pages/dashboards/dashboard1.js"></script>
     <script src="js/custom.js"></script>
     <script src="js/modal.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
     <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
     <script src="https://cdn.datatables.net/2.1.8/js/dataTables.js"></script>
     <script>
